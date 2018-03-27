b1ea16838831c8d5df62da357aeebe0e0523f8c8
Andrew Johnson <andrewjohnson@Andrews-iMac.local>
2018-03-27 08:50:09 -0700

    slight code cleanup and comment tweaks

--- src/card-ai.py
+++ src/card-ai.py
@@ -2,7 +2,7 @@ import random
 
 
 class Game():
-	"""A single game of a CCG-like game."""
+	"""A fantasy card game instance."""
 
 	def __init__(self, players):
 		"""Set the list of players, and set the current_player to the first player."""
@@ -11,7 +11,7 @@ class Game():
 		self.print_moves = False 
 
 	def play_out(self):
-		"""Play out a single game until one or more players is dead."""
+		"""Play out a game until one or more players is at zero hit points."""
 		while not self.game_is_over():
 			player = self.players[self.current_player]
 			if self.print_moves:
@@ -21,14 +21,13 @@ class Game():
 
 	def end_turn(self):
 		"""Check if the game is over. If not, pass to the next player."""
-
 		if self.game_is_over():
 			self.end_game()
 
 		self.current_player = 1 if self.current_player == 0 else 0
 
 	def end_game(self):
-		"""Prints the winner of the game."""
+		"""Print the winner of the game."""
 		if self.game_is_drawn():
 			print "Game Over - Draw"
 
@@ -45,15 +44,15 @@ class Game():
 		print "Game Over - {} {} wins! Final hit points are {} to {}.".format(winner.__class__.__name__, players.index(winner), winning_hp, losing_hp)
 
 	def game_is_drawn(self):
-		"""Returns true if all players has <= 0 hit points."""
+		"""Return true if all players has <= 0 hit points."""
 		return len(self.dead_players()) == len(self.players)
 
 	def game_is_over(self):
-		"""Returns true if either player has <= 0 hit points."""
+		"""Return true if either player has <= 0 hit points."""
 		return len(self.dead_players()) > 0
 
 	def dead_players(self):
-		"""Returns a list of players that have hit points <= 0."""
+		"""Return a list of players that have hit points <= 0."""
 		dead_players = []
 		for p in self.players:
 			if p.hit_points <= 0:
@@ -62,7 +61,7 @@ class Game():
 		return dead_players
 
 	def opponent(self, player):
-		"""Returns the player that isn't the given player."""
+		"""Return the player that isn't the given player."""
 		for p in self.players:
 			if p != player:
 				return p
@@ -70,34 +69,16 @@ class Game():
 		return dead_players
 
 	def strike_player(self, player):
-		"""Remove 3 it points from player."""
-		damage = 3
+		"""Remove 1 hit point from player."""
+		damage = 1
 		player.hit_points -= damage
 		if self.print_moves:
 			print ">>>>{} {} got hit for {} damage!".format(player.__class__.__name__, players.index(player), damage)
 
 	def do_nothing(self, player):
-		"""Do nothing for a turn, as opposed to striking."""
+		"""Do nothing for a turn."""
 		if self.print_moves:
 			print ">>>>{} {} did nothing.".format(player.__class__.__name__, players.index(player))
-
-	def play_random_move(self, player):
-		"""Possible moves are strike self, strike opponent, or do nothing."""
-		possible_moves = [
-							{'method': self.strike_player, 'arg': self.players[0]},
-							{'method': self.strike_player, 'arg': self.players[1]},
-							{'method': self.do_nothing, 'arg': player},
-						  ]
-
-		move_index = random.randint(0,2)
-
-		move = possible_moves[move_index]
-
-		if 'arg' in move:
-			move['method'](move['arg'])
-		else:
-			move['method']()
-
 
 class Bot():
 	"""The base Bot class."""
@@ -125,11 +106,23 @@ class RandomBot(Bot):
 	"""RandomBot plays a random legal move on its turn, then ends the turn,."""
 
 	def play_move(self, game):
-		game.play_random_move(self)
+		"""Play a random move in game. Possible moves are strike self, strike opponent, or do nothing."""
+		possible_moves = [
+							{'method': game.strike_player, 'arg': game.players[0]},
+							{'method': game.strike_player, 'arg': game.players[1]},
+							{'method': game.do_nothing, 'arg': self},
+						  ]
+
+		move_index = random.randint(0,2)
+		move = possible_moves[move_index]
+		if 'arg' in move:
+			move['method'](move['arg'])
+		else:
+			move['method']()
 
 
 if __name__ == "__main__":
-	starting_hit_points = 15
+	starting_hit_points = 2
 
 	for x in range(0, 10000):
 		players = [
